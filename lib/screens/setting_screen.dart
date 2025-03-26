ee0e14ec02b1f669fa4f2b5c643c1b05454754fa
SEKI-YUTA <zard.since1991@gmail.com>
2025-03-26 13:13:46 +0900

    fix: 設定画面をSafeAreaでラップ ipadで見た際にしたのライセンスボタンがめり込みそうだった

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/utils.dart';
 import 'package:introspection_note_mvp/controller/settings_screen_controller.dart';
 
 class SettingsPage extends GetView<SettingsScreenController> {
@@ -11,35 +10,37 @@ class SettingsPage extends GetView<SettingsScreenController> {
     return Obx(
       () => Scaffold(
         appBar: AppBar(title: const Text("設定")),
-        body: Opacity(
-          opacity: controller.isLoading ? 0.5 : 1,
-          child: Column(
-            children: [
-              ListTile(
-                title: const Text("内省のリマインド"),
-                trailing: Switch(
-                  value: controller.enabledRemindNotification,
-                  onChanged: (value) {
-                    controller.toggleRemindNotification(value);
-                  },
+        body: SafeArea(
+          child: Opacity(
+            opacity: controller.isLoading ? 0.5 : 1,
+            child: Column(
+              children: [
+                ListTile(
+                  title: const Text("内省のリマインド"),
+                  trailing: Switch(
+                    value: controller.enabledRemindNotification,
+                    onChanged: (value) {
+                      controller.toggleRemindNotification(value);
+                    },
+                  ),
                 ),
-              ),
-              Spacer(),
-              GestureDetector(
-                onTap: controller.navigateToLicenseScreen,
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    child: Text(
-                      "ライセンス",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16),
+                Spacer(),
+                GestureDetector(
+                  onTap: controller.navigateToLicenseScreen,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: Text(
+                        "ライセンス",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
